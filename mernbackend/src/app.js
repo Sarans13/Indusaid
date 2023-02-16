e51ff1d6a5b627f24a4784e9a0e339f8f21d7e05
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-02-17 04:02:09 +0530

    Update index.hbs
    Co-Authored-By: Kapileswar Rout <93908175+KapileswarRout@users.noreply.github.com>
    Co-Authored-By: Sruti Nayak <105599945+Srutay@users.noreply.github.com>
    Co-Authored-By: Srusti Prusty <115080405+SrustiPrusty@users.noreply.github.com>

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
 const app = express();
 require("./db/conn");
+const Login = require("./models/logins");
 const port = process.env.PORT || 5500;
 
 const static_path = path.join(__dirname,"../public");
@@ -18,7 +20,28 @@ app.get("/login",(req,res)=>{
     res.render("login");
 });
 
+
+app.post("/login", (req,res)=>{
+    try{
+        console.log(req.body.username);
+        res.send(req.body.username);
+    }catch(error){
+        res.status(400).send(error);
+    }
+});
+
+// app.post('/login',(req,res)=>{
+//     var myData = new Login(req.body);
+//     myData.save().then(()=>{
+//         res.send("DATA ranigla");
+//     }).catch(()=>{
+//         res.status(200).send("no item");
+//     })
+//     res.status(200).render('login.hbs');
+// });
+
 app.listen(port,()=>{
     console.log("server started");
 });
+
 
